8f2ab71360278380386f885027f57fb1ce22e899
Can Muhammed KELEK <68104044+canmuhammedkelek@users.noreply.github.com>
2020-07-27 12:42:45 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-function myFunction() {
+document.getElementById('can').addEventListener('click', function() {
     var x = document.getElementById("menu");
     var y = document.getElementById('hmbrgr');
     var z = document.getElementById('logoborder');
+    var w = document.getElementById('can');
 
     if (x.className === "menubar") {
         x.className += " responsive";
         y.className = "fa fa-close";
         z.className = "menubarspan";
+        w.href = "javascript:void(0);";
     } else {
         x.className = "menubar";
         y.className = "fa fa-bars";
         z.className = "";
+        w.href = "";
     }
-}
+});
